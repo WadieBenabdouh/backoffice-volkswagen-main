d3ab6b028260da222861065a9d62e5044f1b6726
WadieBenabdouh <wadie.trade@gmail.com>
2024-06-25 15:43:47 +0100

    medias form page almost done [clean]

--- src/components/pages/Medias/MediaDasboard.jsx
+++ src/components/pages/Medias/MediaDasboard.jsx
@@ -13,9 +13,33 @@ function MediasDashboard() {
             <button>Supprimer</button>
           </div>
 
+          <section className={`${classes.formCard}`}>
+            <h3>Ajouter un média</h3>
+
+            <label htmlFor="ModelID">ID Modèle:</label>
+            <input type="text" />
+
+            <label htmlFor="ModelName">Nom du Modèle:</label>
+            <input type="text" />
+
+            <label htmlFor="URL-one">URL Image 1:</label>
+            <input type="text" />
+
+            <label htmlFor="URL-two">URL Image 2:</label>
+            <input type="text" />
+
+            <label htmlFor="URL-three">URL Image 3:</label>
+            <input type="text" />
+
+            <label htmlFor="URL-brochure">URL Brochure:</label>
+            <input type="text" />
+
+            <button type="submit">Enregistrer</button>
+          </section>
+
           <table>
             <thead>
-            <tr>
+              <tr>
                 <th>ID Modèle</th>
                 <th>Nom du Modèle</th>
                 <th>URL Image 1</th>
@@ -23,19 +47,19 @@ function MediasDashboard() {
                 <th>URL Image 3</th>
                 <th>URL Brochure</th>
                 <th>Actions</th>
-            </tr>
+              </tr>
             </thead>
 
             <tbody id="filledDataTable">
-                <tr>
-                    <th>test</th>
-                    <th>test</th>
-                    <th>test</th>
-                    <th>test</th>
-                    <th>test</th>
-                    <th>test</th>
-                    <th>ACTION BUTTON</th>
-                </tr>
+              <tr>
+                <th>test</th>
+                <th>test</th>
+                <th>test</th>
+                <th>test</th>
+                <th>test</th>
+                <th>test</th>
+                <th>ACTION BUTTON</th>
+              </tr>
             </tbody>
           </table>
         </form>
