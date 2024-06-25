feb3b06c50aae35bfde98faec8e21b1218cd5b3d
WadieBenabdouh <wadie.trade@gmail.com>
2024-06-25 14:32:46 +0100

    save

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+// imports
+import Dashboard from "./components/Dashboard/Dashboard";
+// imports end
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,18 @@
+import classes from "./Dashboard.module.scss";
+
+function Dashboard() {
+  return (
+    <>
+      {/* HEADER */}
+      <header>
+        <h1 className={`${classes.backoffice_Heading}`}>Backoffice Volkswagen</h1>
+      </header>
+      {/* HEADER END*/}
+
+      {/* SIDEBAR */}
+      {/* SIDEBAR END */}
+    </>
+  );
+}
+
+export default Dashboard;
