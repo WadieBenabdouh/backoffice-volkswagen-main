524274a3f0d4c2adc5be2ac68e035e59af54681d
WadieBenabdouh <wadie.trade@gmail.com>
2024-06-26 09:37:12 +0100

    added Firebase config file [stable last commit had N P M install firebase]

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvw6VqBVeP3XWn0lByyxC4TVZjxx8dgRo",
+  authDomain: "backoffice-volkswagen.firebaseapp.com",
+  projectId: "backoffice-volkswagen",
+  storageBucket: "backoffice-volkswagen.appspot.com",
+  messagingSenderId: "461044018413",
+  appId: "1:461044018413:web:845a003061c253c6b870e4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
